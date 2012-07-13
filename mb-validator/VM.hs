33d3a7a83886477506105f33a089759b62c0db7c
Miëtek Bak <mietek@varsztat.com>
2012-07-13 14:30:10 +0100

    Add VM skeleton

---
+++ mb-validator/VM.hs
@@ -0,0 +1,139 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE EmptyDataDecls #-}
+
+module VM where
+
+import Data.ByteString (ByteString)
+import Data.ByteString.Unsafe (unsafeUseAsCStringLen)
+import Foreign.Ptr (Ptr)
+import Foreign.ForeignPtr (ForeignPtr, newForeignPtr, withForeignPtr)
+import Foreign.C.String (CString, castCharToCChar, castCCharToChar, withCString)
+import Foreign.C.Types (CChar (..), CLong (..))
+import Foreign.Marshal.Alloc (alloca, finalizerFree)
+import Foreign.Storable (peek)
+import System.IO.Unsafe (unsafePerformIO)
+
+
+data CState
+type CStatePtr = Ptr CState
+type CStateFPtr = ForeignPtr CState
+data State = State !(CStateFPtr) deriving (Eq, Ord, Show)
+
+unwrapState :: State -> (CStatePtr -> IO a) -> a
+unwrapState (State sfp) action =
+  unsafePerformIO (withForeignPtr sfp action)
+
+wrapState :: CStatePtr -> IO State
+wrapState sp = do
+  sfp <- newForeignPtr finalizerFree sp
+  return (State sfp)
+
+getIntPair :: (CStatePtr -> Ptr CLong -> Ptr CLong -> IO ()) -> State -> (Int, Int)
+getIntPair action s =
+  unwrapState s $ \sp ->
+    alloca $ \ap ->
+      alloca $ \bp -> do
+        action sp ap bp
+        a <- peek ap
+        b <- peek bp
+        return (fromEnum a, fromEnum b)
+
+
+data Move = MLeft | MRight | MUp | MDown | MWait | MAbort deriving (Eq, Ord, Show)
+
+fromMove :: Move -> Char
+fromMove MLeft  = 'L'
+fromMove MRight = 'R'
+fromMove MUp    = 'U'
+fromMove MDown  = 'D'
+fromMove MWait  = 'W'
+fromMove MAbort = 'A'
+
+
+data Condition = CNone | CWin | CLose | CAbort deriving (Enum, Eq, Ord, Show)
+
+
+type Size = (Int, Int)
+type Point = (Int, Int)
+
+
+foreign import ccall unsafe "libvm.h new"
+  cNew :: CLong -> Ptr CChar -> IO CStatePtr
+
+foreign import ccall unsafe "libvm.h new_from_file"
+  cNewFromFile :: CString -> IO CStatePtr
+
+foreign import ccall unsafe "libvm.h dump"
+  cDump :: CStatePtr -> IO ()
+
+foreign import ccall unsafe "libvm.h get_world_size"
+  cGetWorldSize :: CStatePtr -> Ptr CLong -> Ptr CLong -> IO ()
+
+foreign import ccall unsafe "libvm.h get_robot_point"
+  cGetRobotPoint :: CStatePtr -> Ptr CLong -> Ptr CLong -> IO ()
+
+foreign import ccall unsafe "libvm.h get_lift_point"
+  cGetLiftPoint :: CStatePtr -> Ptr CLong -> Ptr CLong -> IO ()
+
+foreign import ccall unsafe "libvm.h get_move_count"
+  cGetMoveCount :: CStatePtr -> CLong
+
+foreign import ccall unsafe "libvm.h get_condition"
+  cGetCondition :: CStatePtr -> CChar
+
+foreign import ccall unsafe "libvm.h make_one_move"
+  cMakeOneMove :: CStatePtr -> CChar -> IO CStatePtr
+
+foreign import ccall unsafe "libvm.h make_moves"
+  cMakeMoves :: CStatePtr -> CString -> IO CStatePtr
+
+
+new :: ByteString -> State
+new input =
+  unsafePerformIO $
+    unsafeUseAsCStringLen input $ \(i, il) -> do
+      sp <- cNew (toEnum il) i
+      wrapState sp
+
+newFromFile :: String -> IO State
+newFromFile path =
+  withCString path $ \p -> do
+    sp <- cNewFromFile p
+    wrapState sp
+
+dump :: State -> IO ()
+dump s =
+  unwrapState s $ \sp ->
+    return (cDump sp)
+
+getWorldSize :: State -> Size
+getWorldSize = getIntPair cGetWorldSize
+
+getRobotPoint :: State -> Point
+getRobotPoint = getIntPair cGetRobotPoint
+
+getLiftPoint :: State -> Point
+getLiftPoint = getIntPair cGetLiftPoint
+
+getMoveCount :: State -> Int
+getMoveCount s =
+  unwrapState s $ \sp ->
+    return (fromEnum (cGetMoveCount sp))
+
+getCondition :: State -> Condition
+getCondition s =
+  unwrapState s $ \sp ->
+    return (toEnum (fromEnum (castCCharToChar (cGetCondition sp))))
+
+makeOneMove :: State -> Move -> State
+makeOneMove s0 move =
+  unwrapState s0 $ \sp0 -> do
+    sp <- cMakeOneMove sp0 (castCharToCChar (fromMove move))
+    wrapState sp
+
+makeMoves :: State -> [Move] -> State
+makeMoves s0 moves =
+  unwrapState s0 $ \sp0 -> do
+    withCString (map fromMove moves) $ \ms -> do
+      sp <- cMakeMoves sp0 ms
+      wrapState sp
---
+++ mb-validator/libvm.c
@@ -0,0 +1,255 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "libvm.h"
+
+
+#define LOG(fmt, ...) \
+    do { \
+        fprintf(stderr, fmt, ##__VA_ARGS__); \
+        fflush(stderr); \
+    } while (0)
+#define LOG_EXIT(fmt, ...) \
+    do { \
+        LOG(fmt, ##__VA_ARGS__); \
+        exit(1); \
+    } while (0)
+#define PERROR_EXIT(msg) \
+    do { \
+        perror(msg); \
+        exit(1); \
+    } while (0)
+#define ASSERT(val) assert(val)
+
+#if 1
+#define DEBUG_LOG(fmt, ...) LOG(fmt, ##__VA_ARGS__)
+#define DEBUG_ASSERT(val) ASSERT(val)
+#else
+#define DEBUG_LOG(fmt, ...)
+#define DEBUG_ASSERT(val)
+#endif
+
+
+#define O_ROBOT       'R'
+#define O_WALL        '#'
+#define O_ROCK        '*'
+#define O_LAMBDA      '\\'
+#define O_CLOSED_LIFT 'L'
+#define O_OPEN_LIFT   'O'
+#define O_EARTH       '.'
+#define O_EMPTY       ' '
+
+#define M_LEFT  'L'
+#define M_RIGHT 'R'
+#define M_UP    'U'
+#define M_DOWN  'D'
+#define M_WAIT  'W'
+#define M_ABORT 'A'
+
+enum {
+    C_NONE,
+    C_WIN,
+    C_LOSE,
+    C_ABORT
+};
+
+struct state {
+    long world_w, world_h;
+    long robot_x, robot_y;
+    long lift_x, lift_y;
+    long move_count;
+    char condition;
+    long world_size;
+    char world[];
+};
+
+
+static void scan_input(long input_size, const char *input, long *out_world_w, long *out_world_h, long *out_world_size) {
+    DEBUG_ASSERT(input && out_world_w && out_world_h);
+    long world_w = 0, world_h = 0, w = 0, i;
+    for (i = 0; i < input_size; i++) {
+        if (input[i] == '\n') {
+            if (w > world_w)
+                world_w = w;
+            world_h++;
+            w = 0;
+        } else
+            w++;
+    }
+    *out_world_w = world_w;
+    *out_world_h = world_h;
+    *out_world_size = (world_w + 1) * world_h + 1;
+}
+
+
+inline static void make_point(struct state *s, long w, long h, long *out_x, long *out_y) {
+    DEBUG_ASSERT(s && out_x && out_y);
+    *out_x = w + 1;
+    *out_y = s->world_h - h;
+}
+
+
+static void copy_input(struct state *s, long input_size, const char* input) {
+    DEBUG_ASSERT(s && input);
+    long w = 0, h = 0, j = 0, i;
+    for (i = 0; i < input_size; i++) {
+        if (input[i] == '\n') {
+            while (w < s->world_w) {
+                s->world[j++] = O_EMPTY;
+                w++;
+            }
+            s->world[j++] = '\n';
+            h++;
+            w = 0;
+        } else {
+            if (input[i] == O_ROBOT)
+                make_point(s, w, h, &s->robot_x, &s->robot_y);
+            else if (input[i] == O_CLOSED_LIFT)
+                make_point(s, w, h, &s->lift_x, &s->lift_y);
+            s->world[j++] = input[i];
+            w++;
+        }
+    }
+    s->world[j] = 0;
+}
+
+
+struct state *new(long input_size, const char *input) {
+    DEBUG_ASSERT(input);
+    long world_w, world_h, world_size;
+    struct state *s;
+    scan_input(input_size, input, &world_w, &world_h, &world_size);
+    if (!(s = malloc(sizeof(struct state) + world_size)))
+        PERROR_EXIT("malloc");
+    memset(s, 0, sizeof(struct state));
+    s->world_w = world_w;
+    s->world_h = world_h;
+    s->world_size = world_size;
+    copy_input(s, input_size, input);
+    return s;
+}
+
+
+struct state *new_from_file(const char *path) {
+    DEBUG_ASSERT(path);
+    int fd;
+    struct stat info;
+    char *input;
+    struct state *s;
+    if ((fd = open(path, O_RDONLY)) == -1)
+        PERROR_EXIT("open");
+    if (fstat(fd, &info) == -1)
+        PERROR_EXIT("fstat");
+    if ((input = mmap(0, info.st_size, PROT_READ, MAP_SHARED, fd, 0)) == (char *)-1)
+        PERROR_EXIT("mmap");
+    s = new(info.st_size, input);
+    munmap(input, info.st_size);
+    close(fd);
+    return s;
+}
+
+
+void dump(const struct state *s) {
+    DEBUG_ASSERT(s);
+    fputs("----------------------------------------------------------------\n", stderr);
+    fprintf(stderr, "world_size  = (%ld, %ld)\n", s->world_w, s->world_h);
+    fprintf(stderr, "robot_point = (%ld, %ld)\n", s->robot_x, s->robot_y);
+    fprintf(stderr, "lift_point  = (%ld, %ld)\n", s->lift_x, s->lift_y);
+    fprintf(stderr, "move_count  = %ld\n", s->move_count);
+    fprintf(stderr, "condition   = %d\n", s->condition);
+    fprintf(stderr, "world_size  = %ld\n\n", s->world_size);
+    fputs(s->world, stderr);
+    fputc('\n', stderr);
+}
+
+
+struct state *copy(const struct state *s0) {
+    DEBUG_ASSERT(s0);
+    struct state *s;
+    if (!(s = malloc(sizeof(struct state) + s0->world_size)))
+        PERROR_EXIT("malloc");
+    memcpy(s, s0, sizeof(struct state) + s0->world_size);
+    return s;
+}
+
+
+void get_world_size(const struct state *s, long *out_world_w, long *out_world_h) {
+    DEBUG_ASSERT(s && out_world_w && out_world_h);
+    *out_world_w = s->world_w;
+    *out_world_h = s->world_h;
+}
+
+
+void get_robot_point(const struct state *s, long *out_robot_x, long *out_robot_y) {
+    DEBUG_ASSERT(s && out_robot_x && out_robot_y);
+    *out_robot_x = s->robot_x;
+    *out_robot_y = s->robot_y;
+}
+
+
+void get_lift_point(const struct state *s, long *out_lift_x, long *out_lift_y) {
+    DEBUG_ASSERT(s && out_lift_x && out_lift_y);
+    *out_lift_x = s->lift_x;
+    *out_lift_y = s->lift_y;
+}
+
+
+long get_move_count(const struct state *s) {
+    DEBUG_ASSERT(s);
+    return s->move_count;
+}
+
+
+char get_condition(const struct state *s) {
+    DEBUG_ASSERT(s);
+    return s->condition;
+}
+
+
+static void unsafe_make_one_move(struct state *s, char move) {
+    DEBUG_ASSERT(s);
+    DEBUG_ASSERT(s->condition == C_NONE);
+    DEBUG_ASSERT(move == M_LEFT || move == M_RIGHT || move == M_UP || move == M_DOWN || move == M_WAIT || move == M_ABORT);
+    if (move == M_LEFT) {
+        ; // TODO
+    } else if (move == M_RIGHT) {
+        ; // TODO
+    } else if (move == M_UP) {
+        ; // TODO
+    } else if (move == M_DOWN) {
+        ; // TODO
+    } else if (move == M_WAIT) {
+        ; // TODO
+    } else {
+        s->condition = C_ABORT;
+    }
+    s->move_count++;
+}
+
+
+struct state *make_one_move(const struct state *s0, char move) {
+    DEBUG_ASSERT(s0);
+    struct state *s;
+    s = copy(s0);
+    unsafe_make_one_move(s, move);
+    return s;
+}
+
+
+struct state *make_moves(const struct state *s0, const char *moves) {
+    DEBUG_ASSERT(s0 && moves);
+    struct state *s;
+    s = copy(s0);
+    while (*moves) {
+        unsafe_make_one_move(s, *moves);
+        moves++;
+    }
+    return s;
+}
