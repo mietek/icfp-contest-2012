48774497c4991ad320f8639b8be08a6943e693d4
Maciej Kotowicz <maciej.kotowicz@pwr.wroc.pl>
2012-07-13 14:26:41 +0200

    validate.sh - validate answare

---
+++ validate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+map=$1
+if [ $# -eq 1 ]; then read sol ; else sol=$2 ; fi
+
+curl 'http://www.undecidable.org.uk/~edwin/cgi-bin/weblifter.cgi' -d "mapfile=contest$map&route=$sol" 2>/dev/null | grep Score|awk -F '<br>' '{print $2,$3}'
