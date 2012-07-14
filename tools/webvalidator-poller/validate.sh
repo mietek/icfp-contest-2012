7f76745a299529a33e74b63319c53b77444d62f0
Aleksander Balicki <balicki.aleksander@gmail.com>
2012-07-15 00:39:59 +0200

    added 10-min-throttled unit-test generator

---
+++ tools/webvalidator-poller/validate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+### usage: ./validate.sh nr ans
+###        echo ans |./validate.sh nr
+
+map=$1
+if [ $# -eq 1 ]; then read sol ; else sol=$2 ; fi
+
+curl 'http://www.undecidable.org.uk/~edwin/cgi-bin/weblifter.cgi' -d "mapfile=$map&route=$sol" 2>/dev/null | grep Score|awk -F '<br>' '{print $2,$3}'
