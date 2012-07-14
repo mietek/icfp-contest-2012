7c6f6df7407b0078cb2841cd87c93878a96429f8
Aleksander Balicki <balicki.aleksander@gmail.com>
2012-07-15 01:17:49 +0200

    fix validate.sh to spit out only the score

--- tools/webvalidator-poller/validate.sh
+++ tools/webvalidator-poller/validate.sh
@@ -5,4 +5,4 @@
 map=$1
 if [ $# -eq 1 ]; then read sol ; else sol=$2 ; fi
 
-curl 'http://www.undecidable.org.uk/~edwin/cgi-bin/weblifter.cgi' -d "mapfile=$map&route=$sol" 2>/dev/null | grep Score|awk -F '<br>' '{print $2,$3}'
+curl 'http://www.undecidable.org.uk/~edwin/cgi-bin/weblifter.cgi' -d "mapfile=$map&route=$sol" 2>/dev/null | grep Score|awk -F '<br>' '{print $2,$3}'|cut -d' ' -f 2
