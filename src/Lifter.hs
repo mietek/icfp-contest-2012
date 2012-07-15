147be413543b169d688f567cb5a015592f2fdd46
U-lfp\jmi <jmi@lfp.(none)>
2012-07-15 19:27:58 +0200

    removed deterministic solver, enhached the randomized one

--- src/Lifter.hs
+++ src/Lifter.hs
@@ -39,27 +39,28 @@ testMovesList s [] = (False, s, [])
 testMovesList s (m:ms) = let (b, s') = testMoves s m in if b then (b, s', m) else testMovesList s (ms)
 
     
-run :: MVar Builder -> State -> [Move]-> IO ()
-run resultV s0 l = goDijkstra s0 l []
+-- run :: MVar Builder -> State -> [Move] -> [Int] -> IO (Int, [Move])
+run s0 ps ms = goDijkstra s0 ps ms []
   where
-    goDijkstra s (m:ms) prefix = do
+    goDijkstra s (p:ps) (m:ms) prefix = do
       let r = getRobotPoint s
       let (wx, wy) = getWorldSize s
       let c = buildCostTable s r
-      flip mapM_ [1..wy] $ \y -> 
-        flip mapM_ [1..wx] $ \x -> myPrint (x == wx) $ getCost c (x,  wy+1-y)
-      let moves = findA s c r (\(fc, ft, fp) -> elem ft [OLambda, OLift Open])
-      print moves
-      hFlush stdout
-      let moves2 = findA s c r (\(fc, ft, fp) -> elem ft [OEarth])
-      let (b, s', answer) =  testMovesList s $ filter (\x -> x /= []) [moves, moves2, [m], [MRight], [MLeft], [MDown], [MUp]]
-      dump s' 
+--      flip mapM_ [1..wy] $ \y -> 
+--        flip mapM_ [1..wx] $ \x -> myPrint (x == wx) $ getCost c (x,  wy+1-y)
+      let moves = findA s c r (\(fc, ft, fp) -> isLambda s fp || isLift s fp)
+      let moves2 = findA s c r (\(fc, ft, fp) -> isTrampoline s fp || isRazor s fp)
+      let moves3 = findA s c r (\(fc, ft, fp) -> isEarth s fp)
+      -- small probability of doing nothing
+      let all = if (length prefix) < p && (p `mod` 3 == 0) then [] else [moves, moves2, moves3]
+      let (b, s', answer) =  testMovesList s $ filter (\x -> x /= [])  $ all ++ [[m], [MRight], [MLeft], [MDown], [MUp]]
+--      dump s' 
       let result = prefix ++ answer
-      print result
-      hFlush stdout
+--      print result
+--      hFlush stdout
       if  (getCondition s')/= CNone || (moves == [] && length(result)>153) 
-        then print result 
-        else goDijkstra s' ms result
+        then return ((getScore s') , result) 
+        else goDijkstra s' ps ms result
 --      modifyMVar_ resultV $ \result ->
 --        return (result `mappend` fromString answer)
 --      goRandom s' ms (prefix++answer)
@@ -70,17 +71,25 @@ handleInterrupt resultV mainT = do
   toByteStringIO B.putStrLn result
   killThread mainT
 
+prepareRun n input = do
+  seed <- newStdGen
+  let ms  = randomRs (1, 4) seed 
+  let ps  = randomRs (0, n) seed
+  result <- run input ps $ map f ms
+  return result
+  where
+       f :: Int -> Move
+       f 1 = MRight
+       f 2 = MLeft
+       f 3 = MUp
+       f  _  = MDown
+
+  
 main :: IO ()
 main = do
-  resultV <- newMVar mempty
-  mainT <- myThreadId
+--  resultV <- newMVar mempty
+--  mainT <- myThreadId
 --  _ <- installHandler sigINT (Catch (handleInterrupt resultV mainT)) Nothing
   input <- B.getContents
-  seed <- newStdGen
-  let ms  = randomRs ('a', 'd') seed
-  run resultV (new input) $ map f ms
-  where
-       f 'a' = MRight
-       f 'b' = MLeft
-       f 'c' = MUp
-       f  _  = MDown
+  let all = [prepareRun (100-i) (new input) | i<-([1..100]::[Int])] 
+  mapM_(\x -> print =<< x) all
