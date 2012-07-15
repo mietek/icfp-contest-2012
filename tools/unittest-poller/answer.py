ea239b5ef644f671b511ddbeb8b71db3a46c7c29
Rafał Rzepecki <divided.mind@gmail.com>
2012-07-15 23:43:06 +0200

    Shaving is the same probability as other moves now.

--- tools/unittest-poller/answer.py
+++ tools/unittest-poller/answer.py
@@ -2,17 +2,12 @@
 import subprocess, sys, os
 from random import choice, random
 
-moves = ['L', 'R', 'U', 'D', 'W']
-
-SHAVE_CHANCE = 0.02
+moves = ['L', 'R', 'U', 'D', 'W', 'S']
 
 def randomAnswer():
     ans = ''
     for i in range(choice(range(150))):
-      if random() < SHAVE_CHANCE:
-        ans += 'S'
-      else:
-        ans += choice(moves)
+          ans += choice(moves)
 
     if choice(range(5)) == 1:
         ans += 'A'
