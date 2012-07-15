6eca7d2623ea3aa78afea4d71afaa5e58be59f60
Rafał Rzepecki <divided.mind@gmail.com>
2012-07-15 23:38:55 +0200

    2% chance of shaving when generating random tests.

--- tools/unittest-poller/answer.py
+++ tools/unittest-poller/answer.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 import subprocess, sys, os
-from random import choice
+from random import choice, random
 
 moves = ['L', 'R', 'U', 'D', 'W']
 
+SHAVE_CHANCE = 0.02
 
 def randomAnswer():
     ans = ''
     for i in range(choice(range(150))):
+      if random() < SHAVE_CHANCE:
+        ans += 'S'
+      else:
         ans += choice(moves)
 
     if choice(range(5)) == 1:
