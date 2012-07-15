e1d705524f266ea1e964e94086783d4b877b8ad8
Rafał Rzepecki <divided.mind@gmail.com>
2012-07-15 21:21:31 +0200

    Colors and no diff in tester output.

--- unittests/runtests.sh
+++ unittests/runtests.sh
@@ -1,13 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-VALIDATOR=${1-./lambdamine}
+VALIDATOR=$1
+GREEN="\033[32m"
+RED="\033[31m"
+PLAIN="\033[0m"
+
+if [ -z "$VALIDATOR" ]; then
+  echo "Argument (validator path) missing."
+  exit 1
+fi
 
 for d in `dirname "$0"`/tests/*; do
   MAP=$d/map
   for f in $d/paths/*; do
     ROUTE=`basename $f`
-    TMP=/tmp/$ROUTE
+    TMP=`tempfile`
     echo "$ROUTE" | $VALIDATOR -vv $MAP > $TMP
-    diff -B $f $TMP && (echo $f ok; rm $TMP) || echo "$f failed; output in $TMP" 
+    if diff -B $f $TMP > /dev/null; then
+      echo -e "$GREEN$f ok $PLAIN"; rm $TMP
+    else
+      echo -e "$RED$f failed!!!$PLAIN"
+    fi
   done
 done
