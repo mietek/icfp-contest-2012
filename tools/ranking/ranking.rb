b38a66a9d1c7b5f3490a6d82a68e4bfb00012992
Rafał Rzepecki <divided.mind@gmail.com>
2012-07-16 08:38:56 +0200

    Better interface for the ranking.

--- tools/ranking/ranking.rb
+++ tools/ranking/ranking.rb
@@ -1,19 +1,46 @@
 #!/usr/bin/env ruby
-# usage: ranking.rb [timeout] [lifter] [validator]
+# usage: ranking.rb [options]
+# options:
+# -t, --timeout <seconds>
+# -l, --lifter <path>
+# -v, --validator <path>
+# -m, --map <name>
 
 BASEDIR = File.dirname(__FILE__)
 $: << BASEDIR
 TOPDIR = File.join(BASEDIR, "..", "..")
 RUNNER = File.join(BASEDIR, "runner.sh")
 
+require 'getoptlong'
 require 'icfp2012'
 require 'open4'
 
-TIMEOUT=(ARGV[0] || 150).to_i
-lifter = ARGV[1] || File.join(TOPDIR, 'bin', 'lifter')
-validator = ARGV[2] || File.join(TOPDIR, 'bin', 'validator')
+opts = GetoptLong.new(
+  [ '--timeout', '-t', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--lifter', '-l', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--validator', '-v', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--map', '-m', GetoptLong::REQUIRED_ARGUMENT ]
+)
+
+timelimit = 150
+lifter = File.join(TOPDIR, 'bin', 'lifter')
+validator = File.join(TOPDIR, 'bin', 'validator')
+
 ranking = ICFP2012::WebRanking.get
 maps = ranking.keys
+
+opts.each do |opt, arg|
+  case opt
+  when '--timeout'
+    timelimit = arg.to_i
+  when '--lifter'
+    lifter = arg
+  when '--validator'
+    validator = arg
+  when '--map'
+    maps = [arg]
+  end
+end
 
 def locate_map map
   File.join(TOPDIR, 'tests', map) + '.map'
@@ -22,15 +49,16 @@ end
 class Lifter
   WAITTIME=10
 
-  def initialize binary
+  def initialize binary, timelimit
     @binary = binary
+    @timelimit = timelimit
   end
   
   def run map
     Open4.popen4 @binary, 'r+' do |pid, stdin, stdout, _|
       stdin.print map
       stdin.close
-      result = IO.select([stdout], [], [], TIMEOUT)
+      result = IO.select([stdout], [], [], @timelimit)
       if result.nil?
         STDERR.print "[timeout]"
         Process.kill('INT', pid)
@@ -42,7 +70,7 @@ class Lifter
   end
 end
 
-lifter = Lifter.new lifter
+lifter = Lifter.new lifter, timelimit
 
 def rank_score ranking, score
   rank = 1
